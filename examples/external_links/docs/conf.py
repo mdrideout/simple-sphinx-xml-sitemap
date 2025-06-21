c2ae6e1ba868e24bd3957ac20bcd8767c710707f
Matthew Rideout <boon4376@gmail.com>
2025-06-21 13:42:27 -0400

    Ignore external toctree links

---
+++ examples/external_links/docs/conf.py
@@ -0,0 +1,4 @@
+project = 'External Links'
+extensions = ['simple_sphinx_xml_sitemap']
+html_baseurl = 'https://example.com/'
+exclude_patterns = ['_build']
---
+++ examples/external_links/docs/index.rst
@@ -0,0 +1,8 @@
+External Links Example
+======================
+
+.. toctree::
+   :maxdepth: 1
+
+   Page <page>
+   External <https://google.com>
